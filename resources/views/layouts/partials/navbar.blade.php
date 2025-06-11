0582fc6e5b62915e8ddef0e9e5f828b245ee6109
DavinMetoti <DavinMetotii@gmail.com>
2025-06-12 04:05:35 +0700

    Refactor code structure for improved readability and maintainability

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="collapse navbar-collapse justify-content-between">
         <div class="navbar-logo">
             <button class="btn navbar-toggler navbar-toggler-humburger-icon hover-bg-transparent" type="button" data-bs-toggle="collapse" data-bs-target="#navbarVerticalCollapse" aria-controls="navbarVerticalCollapse" aria-expanded="false" aria-label="Toggle Navigation"><span class="navbar-toggle-icon"><span class="toggle-line"></span></span></button>
-            <a class="navbar-brand me-1 me-sm-3" href="index.html">
+            <a class="navbar-brand me-1 me-sm-3" href="{{ route('app.dashboard.index') }}">
                 <div class="d-flex align-items-center">
                     <div class="d-flex align-items-center"><img src="{{ asset('assets/img/icons/logo.png') }}" alt="wasic" width="50" />
                         <h5 class="logo-text ms-2 d-none d-sm-block">wasic</h5>
@@ -16,94 +16,22 @@
                     <h6 class="dropdown-header text-body-highlight fs-10 py-2">24 <span class="text-body-quaternary">results</span></h6>
                     <hr class="my-0" />
                     <h6 class="dropdown-header text-body-highlight fs-9 border-bottom border-translucent py-2 lh-sm">Recently Searched </h6>
-                    <div class="py-2"><a class="dropdown-item" href="apps/e-commerce/landing/product-details.html">
-                            <div class="d-flex align-items-center">
-                                <div class="fw-normal text-body-highlight title"><span class="fa-solid fa-clock-rotate-left" data-fa-transform="shrink-2"></span> Store Macbook</div>
-                            </div>
-                        </a>
-                        <a class="dropdown-item" href="apps/e-commerce/landing/product-details.html">
-                            <div class="d-flex align-items-center">
-                                <div class="fw-normal text-body-highlight title"> <span class="fa-solid fa-clock-rotate-left" data-fa-transform="shrink-2"></span> MacBook Air - 13″</div>
-                            </div>
-                        </a>
-                    </div>
                     <hr class="my-0" />
                     <h6 class="dropdown-header text-body-highlight fs-9 border-bottom border-translucent py-2 lh-sm">Products</h6>
-                    <div class="py-2"><a class="dropdown-item py-2 d-flex align-items-center" href="apps/e-commerce/landing/product-details.html">
-                            <div class="file-thumbnail me-2"><img class="h-100 w-100 object-fit-cover rounded-3" src="https://prium.github.io/phoenix/v1.22.0/assets/img/products/60x60/3.png" alt="" /></div>
-                            <div class="flex-1">
-                                <h6 class="mb-0 text-body-highlight title">MacBook Air - 13″</h6>
-                                <p class="fs-10 mb-0 d-flex text-body-tertiary"><span class="fw-medium text-body-tertiary text-opactity-85">8GB Memory - 1.6GHz - 128GB Storage</span></p>
-                            </div>
-                        </a>
-                        <a class="dropdown-item py-2 d-flex align-items-center" href="apps/e-commerce/landing/product-details.html">
-                            <div class="file-thumbnail me-2"><img class="img-fluid" src="https://prium.github.io/phoenix/v1.22.0/assets/img/products/60x60/3.png" alt="" /></div>
-                            <div class="flex-1">
-                                <h6 class="mb-0 text-body-highlight title">MacBook Pro - 13″</h6>
-                                <p class="fs-10 mb-0 d-flex text-body-tertiary"><span class="fw-medium text-body-tertiary text-opactity-85">30 Sep at 12:30 PM</span></p>
-                            </div>
-                        </a>
-                    </div>
                     <hr class="my-0" />
                     <h6 class="dropdown-header text-body-highlight fs-9 border-bottom border-translucent py-2 lh-sm">Quick Links</h6>
-                    <div class="py-2"><a class="dropdown-item" href="apps/e-commerce/landing/product-details.html">
-                            <div class="d-flex align-items-center">
-                                <div class="fw-normal text-body-highlight title"><span class="fa-solid fa-link text-body" data-fa-transform="shrink-2"></span> Support MacBook House</div>
-                            </div>
-                        </a>
-                        <a class="dropdown-item" href="apps/e-commerce/landing/product-details.html">
-                            <div class="d-flex align-items-center">
-                                <div class="fw-normal text-body-highlight title"> <span class="fa-solid fa-link text-body" data-fa-transform="shrink-2"></span> Store MacBook″</div>
-                            </div>
-                        </a>
-                    </div>
                     <hr class="my-0" />
                     <h6 class="dropdown-header text-body-highlight fs-9 border-bottom border-translucent py-2 lh-sm">Files</h6>
-                    <div class="py-2"><a class="dropdown-item" href="apps/e-commerce/landing/product-details.html">
-                            <div class="d-flex align-items-center">
-                                <div class="fw-normal text-body-highlight title"><span class="fa-solid fa-file-zipper text-body" data-fa-transform="shrink-2"></span> Library MacBook folder.rar</div>
-                            </div>
-                        </a>
-                        <a class="dropdown-item" href="apps/e-commerce/landing/product-details.html">
-                            <div class="d-flex align-items-center">
-                                <div class="fw-normal text-body-highlight title"> <span class="fa-solid fa-file-lines text-body" data-fa-transform="shrink-2"></span> Feature MacBook extensions.txt</div>
-                            </div>
-                        </a>
-                        <a class="dropdown-item" href="apps/e-commerce/landing/product-details.html">
-                            <div class="d-flex align-items-center">
-                                <div class="fw-normal text-body-highlight title"> <span class="fa-solid fa-image text-body" data-fa-transform="shrink-2"></span> MacBook Pro_13.jpg</div>
-                            </div>
-                        </a>
-                    </div>
                     <hr class="my-0" />
                     <h6 class="dropdown-header text-body-highlight fs-9 border-bottom border-translucent py-2 lh-sm">Members</h6>
-                    <div class="py-2"><a class="dropdown-item py-2 d-flex align-items-center" href="pages/members.html">
-                            <div class="avatar avatar-l status-online  me-2 text-body">
-                                <img class="rounded-circle " src="https://www.garrickadenbuie.com/blog/process-profile-picture-magick/index_files/figure-html/resized-cropped-1.png" alt="" />
-                            </div>
-                            <div class="flex-1">
-                                <h6 class="mb-0 text-body-highlight title">Carry Anna</h6>
-                                <p class="fs-10 mb-0 d-flex text-body-tertiary">anna@technext.it</p>
-                            </div>
-                        </a>
-                        <a class="dropdown-item py-2 d-flex align-items-center" href="pages/members.html">
-                            <div class="avatar avatar-l  me-2 text-body">
-                                <img class="rounded-circle " src="https://www.garrickadenbuie.com/blog/process-profile-picture-magick/index_files/figure-html/resized-cropped-1.png" alt="" />
-                            </div>
-                            <div class="flex-1">
-                                <h6 class="mb-0 text-body-highlight title">John Smith</h6>
-                                <p class="fs-10 mb-0 d-flex text-body-tertiary">smith@technext.it</p>
-                            </div>
-                        </a>
-                    </div>
                     <hr class="my-0" />
                     <h6 class="dropdown-header text-body-highlight fs-9 border-bottom border-translucent py-2 lh-sm">Related Searches</h6>
-                    <div class="py-2"><a class="dropdown-item" href="apps/e-commerce/landing/product-details.html">
+                    <div class="py-2"><a class="dropdown-item" href="#">
                             <div class="d-flex align-items-center">
                                 <div class="fw-normal text-body-highlight title"><span class="fa-brands fa-firefox-browser text-body" data-fa-transform="shrink-2"></span> Search in the Web MacBook</div>
                             </div>
                         </a>
-                        <a class="dropdown-item" href="apps/e-commerce/landing/product-details.html">
+                        <a class="dropdown-item" href="#">
                             <div class="d-flex align-items-center">
                                 <div class="fw-normal text-body-highlight title"> <span class="fa-brands fa-chrome text-body" data-fa-transform="shrink-2"></span> Store MacBook″</div>
                             </div>
